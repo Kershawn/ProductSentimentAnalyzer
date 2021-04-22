9020aafb62e76d2392cc4fc9ddbe53bc04803ae4
Kershawn <kershawn7700@gmail.com>
2021-04-22 17:20:35 -0400

    Homepage added

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
