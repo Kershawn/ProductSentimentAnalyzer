995734b1672138abaf30ede00a4096ab4f65efaf
Kershawn <kershawn7700@gmail.com>
2021-04-23 00:25:40 -0400

    Model Selector checkbox added

--- pages/fileupload.py
+++ pages/fileupload.py
@@ -2,4 +2,10 @@ import streamlit as st
 
 def app():
     st.title('File Upload')
-    st.write('Welcome to the File Upload Page')
+    #  st.write('Welcome to the File Upload Page')
+
+    st.write(
+"""_Uploaded files must contain the following column which must be labeled in the first row of the file_\n
+`Review`
+""")
+    fp = st.file_uploader('Upload CSV')
--- pages/productanalysis.py
+++ pages/productanalysis.py
@@ -1,5 +1,21 @@
 import streamlit as st
 
 def app():
-    st.title('Positive Products')
+    st.title('Product Analysis')
     st.write('Welcome to Positive Products')
+
+    st.write(
+    """_Uploaded files must contain the following column which must be labeled in the first row of the file_\n
+    `Review`
+    """)
+    fp = st.file_uploader('Upload JSON')
+    if fp is not None:
+        st.write("File uploaded Successfully")
+
+
+    
+
+    st.selectbox("Select a model to process data:",["CD and Vinyl", "Digital Music","Pet Supplies","Industrial and Scientific","Arts and Craft"] )
+
+
+        
