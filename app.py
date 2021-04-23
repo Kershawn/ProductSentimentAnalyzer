53b10d5b6de8ffada6cb3c2b0ad3aeb724ee7111
Kershawn <kershawn7700@gmail.com>
2021-04-23 08:17:46 -0400

    updated requirements and navigation

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pages.productanalysis as pa
-import pages.negativeproducts as np
-import pages.fileupload as fu
 import pages.home as home
 
 # st.title('Home')
@@ -11,9 +9,7 @@ st.sidebar.title('Navigation')
 
 PAGES = {
     "Home": home,
-    "File Upload": fu,
     "Product Analysis": pa,
-    "Negative Products": np
 }
 
 # Add a radio button to the sidebar:
